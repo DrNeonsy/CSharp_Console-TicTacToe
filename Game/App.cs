480a5321905ef7515c3947a46712d9406d7af0b6
Timothy Enderson <timothyenderson01@gmail.com>
2022-11-18 20:53:26 +0100

    Menu Appearance

--- Game/App.cs
+++ Game/App.cs
@@ -2,5 +2,46 @@
 {
     internal class App
     {
+        internal static void StartUp()
+        {
+            Config();
+            Intro();
+
+            static void Config()
+            {
+                Console.CursorVisible = false;
+                Console.Title = "Tic Tac Toe";
+            }
+
+            static void Intro()
+            {
+                Console.WriteLine(Banner.Tic);
+                Thread.Sleep(555);
+                Console.Clear();
+
+                Console.WriteLine(Banner.Tac);
+                Thread.Sleep(555);
+                Console.Clear();
+
+                Console.WriteLine(Banner.Toe);
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+        }
+
+        internal static void Menu()
+        {
+            #region Menu Point Variables
+            string action1 = "SinglePlayer";
+            string action2 = "MultiPlayer";
+            string action3 = "Player Symbols";
+            string action4 = "Toggle Music";
+            string action5 = "Exit";
+            #endregion
+
+            Console.WriteLine("{0,-32}");
+
+            Console.ReadKey();
+        }
     }
 }
--- Game/EntryPoint.cs
+++ Game/EntryPoint.cs
@@ -4,23 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Banner.Tic);
-            Thread.Sleep(555);
-            Console.Clear();
+            App.StartUp(); // Init Stuff
 
-            Console.WriteLine(Banner.Tac);
-            Thread.Sleep(555);
-            Console.Clear();
+            //while (true) App.Menu(); // Game Menu And Everything Else
 
-            Console.WriteLine(Banner.Toe);
-            Thread.Sleep(1000);
-            Console.Clear();
-
-
-            Dev.PreviewRenderField();
-            Console.ReadKey();
-            Settings.Default.MusicActive= true;
-            Settings.Default.Save();
+            App.Menu();
         }
     }
 }
