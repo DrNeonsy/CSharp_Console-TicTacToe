831be150fc49b4e92ea72b6d353acf09d174560c
Timothy Enderson <timothyenderson01@gmail.com>
2022-11-22 16:37:45 +0100

    Fixed Order Of Execution For The Checks

--- Game/Game.cs
+++ Game/Game.cs
@@ -94,13 +94,7 @@
                     #endregion
 
                     #region Check Board
-                    if (CheckTie())
-                    {
-                        gameOngoing = !CheckTie();
-                        winner = 3;
-                        break;
-                    }
-                    else if (CheckWinner())
+                    if (CheckWinner())
                     {
                         gameOngoing = !CheckWinner();
                         if (i == 1)
@@ -113,6 +107,12 @@
                             winner = 2;
                             break;
                         }
+                    }
+                    else if (CheckTie())
+                    {
+                        gameOngoing = !CheckTie();
+                        winner = 3;
+                        break;
                     }
                     #endregion
                 }
